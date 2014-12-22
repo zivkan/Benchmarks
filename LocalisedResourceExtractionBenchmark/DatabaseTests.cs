73bcbdd7f9e393e7cd1646c556a08d302dd7d19f
Andy Zivkovic <zivkovic.andy@gmail.com>
2014-12-22 22:39:58 +0100

    Create tables

---
+++ LocalisedResourceExtractionBenchmark/DatabaseTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LocalisedResourceExtractionBenchmark
+{
+    [TestClass]
+    public class DatabaseTests
+    {
+        private const string ConnectionString = "Data Source=(LocalDB)\\ProjectsV12;Integrated Security=true";
+
+        [TestMethod]
+        public void CreateTablesAndData()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                Console.WriteLine(connection.ConnectionString);
+
+                CreateTables(connection);
+            }
+        }
+
+        private void CreateTables(SqlConnection connection)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE Dictionary (" +
+                                      "Id INT, " +
+                                      "Lang CHAR(2), " +
+                                      "Label NVARCHAR(MAX) NOT NULL,"+
+                                      "PRIMARY KEY (Id,Lang))";
+                command.ExecuteNonQuery();
+
+                command.CommandText = "CREATE TABLE Source (" +
+                                      "Id INT PRIMARY KEY IDENTITY, " +
+                                      "Code VARCHAR(50) NOT NULL, " +
+                                      "Parent INT," +
+                                      "Labels INT NOT NULL)";
+                command.ExecuteNonQuery();
+
+            }
+        }
+    }
+}
