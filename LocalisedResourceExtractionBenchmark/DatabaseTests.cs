797bd82b89edeafd8db324b47606eea2218c80a4
Andy Zivkovic <zivkovic.andy@gmail.com>
2014-12-25 11:19:33 +0100

    Add labels as columns extraction

--- LocalisedResourceExtractionBenchmark/DatabaseTests.cs
+++ LocalisedResourceExtractionBenchmark/DatabaseTests.cs
@@ -10,65 +10,17 @@ namespace LocalisedResourceExtractionBenchmark
     {
         private const string ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;Database=LocalisedResourceExtractionBenchmark;Integrated Security=true";
 
-        [TestMethod, Ignore]
+        [TestMethod]
         public void CreateTablesAndData()
         {
             using (var connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
 
-                Console.WriteLine(connection.ConnectionString);
+                var langs = new[] {"en", "fr", "de", "it", "es"};
 
-                CreateTables(connection);
-
-                PopulateTables(connection);
-            }
-        }
-
-        private void CreateTables(SqlConnection connection)
-        {
-            using (var command = connection.CreateCommand())
-            {
-                command.CommandText = "CREATE TABLE Dictionary (" +
-                                      "Id INT NOT NULL, " +
-                                      "Lang CHAR(2) NOT NULL, " +
-                                      "Label NVARCHAR(MAX) NOT NULL,"+
-                                      "PRIMARY KEY (Id,Lang))";
-                command.ExecuteNonQuery();
-
-                command.CommandText = "CREATE TABLE Source (" +
-                                      "Id INT IDENTITY NOT NULL, " +
-                                      "Code VARCHAR(50) NOT NULL, " +
-                                      "Parent INT," +
-                                      "Labels INT NOT NULL,"+
-                                      "PRIMARY KEY(Id))";
-                command.ExecuteNonQuery();
-
-            }
-        }
-
-        private void PopulateTables(SqlConnection connection)
-        {
-            const int maxRows = 500000;
-            var dictionary = new TestDataBulkReader(TestDataBulkReader.Table.Dictionary, maxRows);
-            var source = new TestDataBulkReader(TestDataBulkReader.Table.Source, maxRows);
-
-            using (var bulkCopy = new SqlBulkCopy(connection))
-            {
-                bulkCopy.BatchSize = 100000;
-                bulkCopy.DestinationTableName = "Dictionary";
-                bulkCopy.ColumnMappings.Add("Id", "Id");
-                bulkCopy.ColumnMappings.Add("Lang", "Lang");
-                bulkCopy.ColumnMappings.Add("Label", "Label");
-                bulkCopy.WriteToServer(dictionary);
-
-                bulkCopy.DestinationTableName = "Source";
-                bulkCopy.ColumnMappings.Clear();
-                bulkCopy.ColumnMappings.Add("Id", "Id");
-                bulkCopy.ColumnMappings.Add("Code", "Code");
-                bulkCopy.ColumnMappings.Add("Parent", "Parent");
-                bulkCopy.ColumnMappings.Add("Labels", "Labels");
-                bulkCopy.WriteToServer(source);
+                var db = new DbInitialise(connection, 500000, langs);
+                db.CreateAndPopulateTables();
             }
         }
 
@@ -127,6 +79,18 @@ namespace LocalisedResourceExtractionBenchmark
                 connection.Open();
 
                 ISourceRepository repo = new LabelsAsXml(connection);
+                RunTest(repo);
+            }
+        }
+
+        [TestMethod]
+        public void LabelsAsColumnsTests()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                ISourceRepository repo = new LabelsAsColumns(connection);
                 RunTest(repo);
             }
         }
---
+++ LocalisedResourceExtractionBenchmark/DbInitialise.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LocalisedResourceExtractionBenchmark
+{
+    class DbInitialise
+    {
+        private readonly SqlConnection _connection;
+        private readonly int _numSourcers;
+        private readonly IList<string> _languages;
+
+        public DbInitialise(SqlConnection connection, int numSourcers, IList<string> languages)
+        {
+            _connection = connection;
+            _numSourcers = numSourcers;
+            _languages = languages;
+        }
+
+        public void CreateAndPopulateTables()
+        {
+            CreateLanguageTable();
+            CreateDictionaryTable();
+            CreateSourceTable();
+
+            PopulateLanguageTable();
+            PopulateDictionaryTable();
+            PopulateSourceTable();
+        }
+
+        private void CreateLanguageTable()
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE Languages (Lang CHAR(2) NOT NULL PRIMARY KEY)";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void CreateDictionaryTable()
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE Dictionary (" +
+                                      "Id INT NOT NULL, " +
+                                      "Lang CHAR(2) NOT NULL, " +
+                                      "Label NVARCHAR(MAX) NOT NULL," +
+                                      "PRIMARY KEY (Id,Lang)," +
+                                      "FOREIGN KEY (Lang) REFERENCES Languages(Lang))";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void CreateSourceTable()
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "CREATE TABLE Source (" +
+                                      "Id INT IDENTITY NOT NULL, " +
+                                      "Code VARCHAR(50) NOT NULL, " +
+                                      "Parent INT," +
+                                      "Labels INT NOT NULL," +
+                                      "PRIMARY KEY(Id))";
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void PopulateLanguageTable()
+        {
+            using (var command = _connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Languages (Lang) VALUES (@lang)";
+                var param = command.Parameters.Add(command.CreateParameter());
+                param.ParameterName = "@lang";
+                foreach (var lang in _languages)
+                {
+                    param.Value = lang;
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void PopulateDictionaryTable()
+        {
+            using (var bulk = new SqlBulkCopy(_connection))
+            {
+                var dictionary = new TestDataBulkReader(TestDataBulkReader.Table.Dictionary, _numSourcers);
+
+                bulk.BatchSize = 100000;
+                bulk.DestinationTableName = "Dictionary";
+                bulk.ColumnMappings.Add("Id", "Id");
+                bulk.ColumnMappings.Add("Lang", "Lang");
+                bulk.ColumnMappings.Add("Label", "Label");
+                bulk.WriteToServer(dictionary);
+            }
+        }
+
+        private void PopulateSourceTable()
+        {
+            using (var bulk = new SqlBulkCopy(_connection))
+            {
+                var source = new TestDataBulkReader(TestDataBulkReader.Table.Source, _numSourcers);
+
+                bulk.BatchSize = 100000;
+                bulk.DestinationTableName = "Source";
+                bulk.ColumnMappings.Add("Id", "Id");
+                bulk.ColumnMappings.Add("Code", "Code");
+                bulk.ColumnMappings.Add("Parent", "Parent");
+                bulk.ColumnMappings.Add("Labels", "Labels");
+                bulk.WriteToServer(source);
+            }
+        }
+
+    }
+}
